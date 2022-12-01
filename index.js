58beeecd6559405951c85c2e2312f3ecf999efde
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2022-11-30 22:58:29 -0400

    Implementación de sequelize y env

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from "express";
 import usuariosRoutes from "./routes/usuariosRoutes.js";
+import db from "./config/db.js";
 
 //* Crear app
 const app = express();
+
+//* Conexión a la db
+try {
+    await db.authenticate();
+    console.log('Conexión exitosa');
+} catch (error) {
+    console.log(error);
+}
 
 //* Definir puerto
 const port = 4000;
@@ -16,7 +25,6 @@ app.use(express.static('public'));
 
 //* Routing
 app.use('/auth', usuariosRoutes);
-
 
 app.listen(port, () => {
     console.log(`El servidor esta funcionando en el puerto ${port}`);
