238b4b052d5375dbd4a45e39eeae293887812a14
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2022-11-30 23:39:23 -0400

    Se agrego una ruta method POST register

--- routes/usuariosRoutes.js
+++ routes/usuariosRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { formularioLogin, formularioOlvido, formularioRegister } from "../controllers/usuarioControllers.js";
+import { formularioLogin, formularioOlvido, formularioRegister, registrar } from "../controllers/usuarioControllers.js";
 
 const router = express.Router();
 
 router.get('/login', formularioLogin);
 router.get('/register', formularioRegister);
+router.post('/register', registrar);
 router.get('/olvide', formularioOlvido);
 
 export default router;
