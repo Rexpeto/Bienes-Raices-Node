a2df35e9871ebd5f27efbaf5bbffe4098a1141ac
Carlos <Carlosgallardoparra16@gmail.com>
2022-12-03 16:13:04 -0400

    Se corrigio el export

--- models/Categoria.js
+++ models/Categoria.js
@@ -1,9 +1,11 @@
 import { DataTypes } from "sequelize";
 import db from '../config/db.js';
 
-export const Categoria = db.define('categorias', {
+const Categoria = db.define('categorias', {
     nombre: {
         type: DataTypes.STRING(30),
         allowNull: false
     }
 });
+
+export default Categoria;
