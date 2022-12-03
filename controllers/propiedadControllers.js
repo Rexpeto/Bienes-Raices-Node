58cbd1ccdb2ef8d7b1cdafb62484dd91d72f1023
Carlos <Carlosgallardoparra16@gmail.com>
2022-12-03 16:14:11 -0400

    Se agrego las consultas a modelos

--- controllers/propiedadControllers.js
+++ controllers/propiedadControllers.js
@@ -1,3 +1,6 @@
+import Precio from '../models/Precio.js';
+import Categoria from '../models/Categoria.js';
+
 export const admin = (req, res) => {
     res.render('../views/propiedades/admin.pug', {
         pagina: 'Mis propiedades',
@@ -6,9 +9,16 @@ export const admin = (req, res) => {
 }
 
 //* Formulario para crear
-export const crear = (req, res) => {
+export const crear = async (req, res) => {
+    //? Consultar modelo de precio y categoria
+    const [precios, categorias] = await Promise.all([
+        Precio.findAll(),
+        Categoria.findAll()
+    ]);
     res.render('../views/propiedades/crear.pug', {
         pagina: 'Crear Propiedad',
-        barra: true
+        barra: true,
+        categorias,
+        precios
     });
 }
