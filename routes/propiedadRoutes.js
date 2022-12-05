d7d7ae7b60a8a5c42cf4ec2142a6c79ef39bbbf0
Carlos <Carlosgallardoparra16@gmail.com>
2022-12-05 11:01:56 -0400

    Se agregó una ruta post de crear

--- routes/propiedadRoutes.js
+++ routes/propiedadRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { admin, crear } from '../controllers/propiedadControllers.js';
+import { admin, crear, guardar } from '../controllers/propiedadControllers.js';
 
 const router = express.Router();
 
 router.get('/mis-propiedades', admin);
 router.get('/propiedad/crear', crear);
+router.post('/propiedad/crear', guardar);
 
 export default router;
