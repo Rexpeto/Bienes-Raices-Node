a9ab1a0a332423fd9d77d4e7a46b523f3c34c3b9
Carlos <Carlosgallardoparra16@gmail.com>
2022-12-05 10:53:27 -0400

    Se creo asociación id_categoria en propiedad

--- models/index.js
+++ models/index.js
@@ -3,7 +3,11 @@ import Propiedad from './Propiedad.js';
 import Precio from './Precio.js';
 import Usuario from './Usuario.js';
 
-Precio.hasOne(Propiedad);
+//? Crea asociación 1:1 Propiedad tiene un precio
+Precio.hasOne(Propiedad, {foreignKey: 'id_precio'});
+
+//? Crea asociación 1:1 Propiedad tiene una categoria
+Categoria.hasOne(Propiedad, {foreignKey: 'id_categoria'});
 
 export {
     Categoria,
