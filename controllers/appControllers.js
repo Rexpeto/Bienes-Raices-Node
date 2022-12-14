7c2de6fd79dcde7fd83c51779e02b2ee9c7f8fea
Carlos <Carlosgallardoparra16@gmail.com>
2022-12-13 21:57:27 -0400

    Se desarrollo el 404

--- controllers/appControllers.js
+++ controllers/appControllers.js
@@ -36,12 +36,36 @@ export const inicio = async (req, res) => {
     });
 }
 
-export const categoria = (req, res) => {
+export const categoria = async (req, res) => {
+    const {id} = req.params;
 
+    //? Comprobar que exista la categoria
+    const categoria = await Categoria.findByPk(id);
+
+    if(!categoria) {
+        return res.redirect('/404');
+    }
+
+    //? Obtener la propiedad con la categoria
+    const propiedades = await Propiedad.findAll({
+        where: {
+            id_categoria: id
+        },
+        include: [
+            {model: Precio, as: 'precio'}
+        ]
+    });
+
+    res.render('categoria', {
+        pagina: `Categoría de ${categoria.nombre}`,
+        propiedades
+    });
 }
 
 export const notFound = (req, res) => {
-
+    res.render('404', {
+        pagina: 'No encontrado'
+    });
 }
 
 export const buscador = (req, res) => {
