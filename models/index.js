6819e33c578de12668782a9e25e4bb3d7328a992
Rexpeto <Carlosgallardoparra@hotmail.com>
2022-12-22 15:28:12 -0400

    Asociación 1:1 en el modelo Mensaje

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@ import Categoria from './Categoria.js';
 import Propiedad from './Propiedad.js';
 import Precio from './Precio.js';
 import Usuario from './Usuario.js';
+import Mensaje from './Mensaje.js';
 
 //? Crea asociación 1:1 Propiedad tiene un precio
 Propiedad.belongsTo(Precio, {foreignKey: 'id_precio'});
@@ -12,9 +13,13 @@ Propiedad.belongsTo(Categoria, {foreignKey: 'id_categoria'});
 //? Crea asociación 1:1 Propiedad tiene un usuario
 Propiedad.belongsTo(Usuario, {foreignKey: 'id_usuario'});
 
+//? Crea asosiación 1:1 Mensaje tiene una propiedad
+Mensaje.belongsTo(Propiedad, {foreignKey: 'id_propiedad'});
+
 export {
     Categoria,
     Propiedad,
     Precio,
-    Usuario
+    Usuario,
+    Mensaje
 }
