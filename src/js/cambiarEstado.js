ad51a62ea3400eab976af63ec99fb4a1d5b776ad
Rexpeto <Carlosgallardoparra@hotmail.com>
2022-12-22 20:53:07 -0400

    Source de cambiar estado

---
+++ src/js/cambiarEstado.js
@@ -0,0 +1,38 @@
+(function() {
+    const btn = document.querySelectorAll('.cambiar-estado');
+    const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+    btn.forEach(boton => {
+        boton.addEventListener('click', cambiarEstado);
+    })
+
+    async function cambiarEstado(e) {
+        const {change:id} = e.target.dataset;
+        const url = `/propiedades/${id}`;
+
+        try {
+            const respuesta = await fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'CSRF-Token': token
+                }
+            })
+
+            const {resultado} = await respuesta.json()
+
+            if(resultado) {
+                if(e.target.classList.contains('bg-yellow-100')) {
+                    e.target.classList.remove('bg-yellow-100', 'text-yellow-800');
+                    e.target.classList.add('bg-green-100', 'text-green-800');
+                    e.target.textContent = 'Publicado'
+                } else {
+                    e.target.classList.remove('bg-green-100', 'text-green-800');
+                    e.target.classList.add('bg-yellow-100', 'text-yellow-800');
+                    e.target.textContent = 'No publicado'
+                }
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+})()
